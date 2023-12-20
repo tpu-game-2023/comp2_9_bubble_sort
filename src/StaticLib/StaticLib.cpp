56de969c0af144723a9be4094a15441a497a95e7
soltshio <shanshanshien@gmail.com>
2023-12-20 22:02:01 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,34 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
+	if(begin==0||end==0||begin>end)
 	return false;
+
+	else
+	{
+		int change = 0;
+		item copy;
+		int n;
+		n = end - begin;
+
+		if (n > 1)
+		{
+			do
+			{
+				change = 0;
+				for (int i = 0; i < n - 1; i++)
+				{
+					if ((begin + i)->key > (begin + +i + 1)->key)
+					{
+						change = 1;
+						copy = *(begin + i);
+						*(begin + i) = *(begin + i + 1);
+						*(begin + i + 1) = copy;
+					}
+				}
+			} while (change);
+		}
+		
+		return true;
+	}
 }
