928a5d27cf526c2ee0a7d3b58a21b2290cf21930
arukapega <dragon.night3579@gmail.com>
2023-12-20 15:51:09 +0900

    書き換えました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,8 +3,28 @@
 
 #include "../include/lib_func.h"
 
+void sortSwap(item* item1, item* item2)
+{
+	if (item1->key < item2->key)
+		return;
+	item swap;
+	memcpy(&swap, item1, sizeof(*item1));
+	memcpy(item1, item2, sizeof(*item2));
+	memcpy(item2, &swap, sizeof(swap));
+}
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if(begin == NULL || end == NULL) return false;
+	int num = end - begin;
+	if(num < 1) return false;
+
+	for (int i = 0; i < num - 1; i++)
+	{
+		for (int j = 0; j < num - i - 1;j++)
+		{
+			sortSwap(&begin[num - j - 2], &begin[num - j - 1]);
+		}
+	}
+	return true;
 }
