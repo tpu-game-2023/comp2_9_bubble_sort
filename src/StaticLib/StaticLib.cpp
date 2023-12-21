f0a21719c84b745f63f406b2b7611728bd111a9c
mana0629 <ysangyuan885@gmail.com>
2023-12-22 03:08:29 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,29 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL || begin >= end) return false;
+
+	int const FLG_ON = 1;
+	int const FLG_OFF = 0;
+	int const N = end - begin;
+    int n = 1, flag;
+    item tmp;
+    int size = sizeof(tmp);
+
+    do
+    {
+        flag = FLG_OFF;
+        for (int i = 0; i < N - n; i++) {
+            if (begin[i].key > begin[i + 1].key) {
+                flag = FLG_ON;
+
+                memcpy(&tmp, begin + i, size);
+                memcpy(begin + i, begin + i + 1, size);
+                memcpy(begin + i + 1, &tmp, size);
+            }
+        }
+        n++;
+    } while (flag == FLG_ON);
+    return true;
 }
+ 
