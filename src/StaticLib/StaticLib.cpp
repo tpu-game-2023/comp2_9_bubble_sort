3b9fccbfa9848e7835e05c47adb6ef5bc6cb293b
yochi513 <yojrdkt@gmail.com>
2023-12-24 00:41:30 +0900

    テストエクスプローラーがうまく反応しませんでしたが実装しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,21 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (degin == NULL || end == NELL || degin >= end)
+	{
+		return false;
+	}
+    for (int i = begin; i < end - 1; ++i)
+    {
+        for (int j = begin; j < end - 1; ++j)
+        {
+            if (j > (j + 1)) 
+            {
+                int temp = j;
+                j = (j + 1);
+                (j + 1) = temp;
+            }
+        }
+    }
+	return true;
 }
