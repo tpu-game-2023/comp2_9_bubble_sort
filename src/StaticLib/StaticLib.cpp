6f49f119c9fae2025bc55051d5d11839d15acd5e
ayuzakana84 <ayunume@gmail.com>
2023-12-25 00:37:30 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,31 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
+	if (begin != 0 && end != 0 && begin <= end)
+	{
+		for (item* i = begin; i < end - 1; i++)
+		{
+			int changeFlag = 0;
+
+			for (item* j = begin; j < end - 1; j++)
+			{
+				if (j->key > (j + 1)->key)
+				{
+					item num = *(j);
+					*(j) = *(j + 1);
+					*(j + 1) = num;
+
+					changeFlag = 1;
+				}
+			}
+			if (changeFlag == 0)
+			{
+				break;
+			}
+		}
+
+		return true;
+	}
+
 	return false;
 }
