d622a470ead2d5734caa636c4b3b3925294b8a0d
VelDLumina2942 <145996670+VelDLumina2942@users.noreply.github.com>
2023-12-24 17:14:10 +0900

    テスト通せませんでした
    入れ替えのところがやり方を調べていろいろ試したのですが、うまく実装させることができませんでした。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -2,9 +2,27 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
-
+#include<stdlib.h>
+#include<string.h>
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL)
+		return false;
+	else if (begin > end)
+		return false;
+	else
+	{
+		int n = (end - begin) /sizeof(item_*);
+		for (int i = n - 1; i > 0 ; i--) {
+			for (int j = 0;j<n-1 ; j++) {
+				if (begin[j].key>begin[j-1].key)
+				{
+					item_ temp = begin[j];
+					begin[j] = begin[j-1];
+					begin[j-1] = temp;
+				}
+			}
+		}
+	}
 }
