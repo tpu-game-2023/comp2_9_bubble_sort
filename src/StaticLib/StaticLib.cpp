0521d5a30424e5351995ec6b89d466ce49594bb8
diceroll5656 <kuni0521iku@icloud.com>
2024-01-22 16:01:15 +0900

    コミット

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,13 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end < begin)return false;
+
+	for (item* p = begin; p != end - 1; p++) //並び替えのための基礎事項
+	{
+		for(item*q=begin)//さらに順番を変えるための方法
+
+
+	}
+	return true;
 }
